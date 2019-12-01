b6ab216198b9872c34cb70493666dc5612976454
Mattias Bosved <mattias.bosved@eniro.com>
2019-12-01 20:35:18 +0100

    styled components

---
+++ src/App/WelcomeMessage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function WelcomeMessage() {
+  return (
+    <div>
+      <h1>Welcome to CryptoDash</h1>
+    </div>
+  );
+}
+
+export default WelcomeMessage;
--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import WelcomeMessage from './WelcomeMessage';
 import './App.css';
+import styled, { css } from 'styled-components';
 
 function App() {
-  return <div>hello wolrd</div>;
+  return (
+    <div>
+      <WelcomeMessage />
+    </div>
+  );
 }
 
 export default App;
